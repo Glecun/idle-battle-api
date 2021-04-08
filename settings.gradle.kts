c8336316f4433cdef74294b19a24f56766a2350c
Glecun <gregoire.lecun@gmail.com>
2021-04-08 19:59:09 +0200

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "idle-battle-api"
---
+++ src/test/kotlin/com/glecun/idlebattleapi/IdleBattleApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glecun.idlebattleapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdleBattleApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
